960cde608a8747aac43c540b48968297670a4f35
BeeAwwad <aosalihu@gmail.com>
2024-03-18 09:56:53 +0100

    Main-sectionOne: created the main section of the main element

--- src/components/react/sections/sectionOne.jsx
+++ src/components/react/sections/sectionOne.jsx
@@ -1,5 +1,31 @@
+import { Button } from "@/components/ui/button"
 function SectionOne() {
-  return <div>sectionOne</div>
+  return (
+    <section className="md:flex md:flex-row mt-10 px-5 py-8 h-fit md:max-h-[400px] lg:max-h-[500px]">
+      <div className="hidden md:block flex-1">
+        <img
+          className="w-full h-full"
+          src="/images/section/section-1.svg"
+          alt="mobile_app_image"
+        />
+      </div>
+      <div className="flex flex-col justify-between flex-1">
+        <h1 className="text-5xl lg:text-7xl font-bold max-w-[600px]">
+          Simplify your finance with Paywave.
+        </h1>
+        <p className="mt-7 md:text-lg lg:text-2xl max-w-[450px]">
+          Manage all your transactions on one platform. instant, unified and
+          simple with Paywave
+        </p>
+        <div className="flex mt-7">
+          <Button className="text-md text-[#002C6E] bg-white border-[#002C6E] border-2 hover:bg-[#002C6E] hover:text-white">
+            Open an account
+          </Button>
+          <Button className="text-base bg-[#002C6E] ml-5">Get the app</Button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default SectionOne
