3d3536a68820258ee3ffc373fc4e1992925ab80e
BeeAwwad <aosalihu@gmail.com>
2024-06-04 09:21:25 +0100

    GetApp page: Make get-app page fully responsive

--- src/components/react/getApp/contentGetApp.jsx
+++ src/components/react/getApp/contentGetApp.jsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
 function ContentGetApp() {
   return (
-    <div className="h-full font-poppins bg-[url('/images/get-app/background-small.svg')] md:bg-[url('/images/get-app/background.svg')] bg-no-repeat sm:bg-cover bg-center bg-fixed flex items-center justify-center">
+    <div className="h-full font-poppins bg-[url('/images/get-app/background-small.svg')] md:bg-[url('/images/get-app/background.svg')] bg-no-repeat sm:bg-cover bg-center bg-fixed flex items-center justify-center md:items-start">
       <div className="flex flex-col items-center justify-center py-16 px-9 h-fit text-center">
-        <h1 className="text-5xl font-bold text-[#002c6e] mb-5 mt-14">
+        <h1 className="text-5xl md:text-6xl font-bold md:font-extrabold text-[#002c6e] mb-5 mt-14 md:mt-5">
           Get The Paywave App
         </h1>
         <p className="text-xl font-medium mb-5">
@@ -22,7 +22,7 @@ function ContentGetApp() {
           <input
             type="text"
             placeholder="Enter your email or phone number"
-            className="w-3/4 px-4 py-2 bg-[#DCEAFF] rounded-full focus:outline-none ease-in focus:scale-105 placeholder:text-[#002C6E]"
+            className="w-2/4 px-4 py-2 bg-[#DCEAFF] rounded-full focus:outline-none ease-in focus:scale-105 placeholder:text-[#002C6E]"
           />
           <Button className="bg-[#002C6E] text-white hover:bg-[#327CEB] ease-in group focus:scale-105  hover:scale-105 rounded-full p-4 size-12">
             <ArrowRight className="size-6" />
